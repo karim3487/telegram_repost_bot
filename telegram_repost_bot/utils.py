f7ca6c1140d1fe6b3440f181e83ee0922a0482b5
Karim Kabirov <karim3487@gmail.com>
2024-04-04 19:35:31 +0300

    Fix logger name

--- telegram_repost_bot/utils.py
+++ telegram_repost_bot/utils.py
@@ -79,16 +79,16 @@ def parse_post(message: Message) -> Union[tuple[str, str], None]:
         content = remove_hashtags(content)
         content = content.split("\n", 1)[1].strip()
 
-        utils_logger.info(f"Parsed post: Title='{title}', Content='{content}'")
+        logger.info(f"Parsed post: Title='{title}', Content='{content}'")
 
         return title, content
     except TypeError as e:
         error_message = f"TypeError occurred: {e}. Message='{text}'."
-        utils_logger.error(error_message, exc_info=True)
+        logger.error(error_message, exc_info=True)
         raise TypeError(error_message)
     except IndexError as e:
         error_message = f"IndexError occurred: {e}. Message='{text}'."
-        utils_logger.error(error_message, exc_info=True)
+        logger.error(error_message, exc_info=True)
         raise ValueError(error_message)
 
 
@@ -105,7 +105,7 @@ def is_post(text: str, hashtag_ru: str, hashtag_kg: str) -> bool:
         bool: True if the text contains hashtags, False otherwise.
     """
     has_hashtags = hashtag_ru in text or hashtag_kg in text
-    utils_logger.info(f"Post has hashtags: {has_hashtags}")
+    logger.info(f"Post has hashtags: {has_hashtags}")
     return has_hashtags
 
 
